3d6c909d103e25e0843630cba8d8dcd5ab556ff3
Tirtha Sharma <tirthagenze121@gmail.com>
2024-06-18 09:38:05 +0530

    Update remove-duplicates-from-sorted-array.java
    Added Brute Force / Naive and Efficient Approach

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,25 +1,24 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        // Brute Force / Naive Approach => Time Complexity = O(n), Space Complexity = O(n)
-        // int n = nums.length;
-        // int[] temp = new int[n]; // extra space
-        // temp[0] = nums[0];
-        // int result = 1;
-        // for(int i=1;i<n;i++){
-        //     if(temp[result-1]!=nums[i]){
-        //         temp[result] = nums[i];
-        //         result++;
-        //     }
-        // }
+        // Approach - 1 ==> Brute Force / Naive Approach => Time Complexity = O(n), Space Complexity = O(n)
+        int n = nums.length;
+        int[] temp = new int[n]; // extra space
+        temp[0] = nums[0];
+        int result = 1;
+        for(int i=1;i<n;i++){
+            if(temp[result-1]!=nums[i]){
+                temp[result] = nums[i];
+                result++;
+            }
+        }
 
-        // for(int i=0;i<result;i++){
-        //     nums[i] = temp[i];
-        // }   
+        for(int i=0;i<result;i++){
+            nums[i] = temp[i];
+        }   
 
-        // return result;
+        return result;
 
-        // Efficient Approach => Time Complexity = O(n), Space Complexity = O(1)
-
+        // Approach - 2 ==> Efficient Approach => Time Complexity = O(n), Space Complexity = O(1)
         int result = 1;
         for(int i=1;i<nums.length;i++){
             if(nums[i]!=nums[result-1]){

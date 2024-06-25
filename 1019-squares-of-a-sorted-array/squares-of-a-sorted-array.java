13f95f0afa3f7d63cca2a3eb6419cb5c9837186e
Tirtha Sharma <tirthagenze121@gmail.com>
2024-06-25 20:56:50 +0530

    Update squares-of-a-sorted-array.java
    Added the two approaches:- 
    
    1) Time Complexity => O(n + n^2), Space Complexity => O(n)
    2) Time Complexity => O(n), Space Complexity => O(1)

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,21 +1,24 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        // Time Comnplexity - O(n + n^2), Space Complexity - O(n) [Extra space is being used]
-    //     int[] temp = new int[nums.length];
-    //     for(int i=0;i<nums.length;i++){
-    //        temp[i] = nums[i]*nums[i];
-    //     }
-    //     for(int i=0;i<temp.length;i++){
-    //         for(int j=i+1;j<temp.length;j++){
-    //             if(temp[i] > temp[j]){
-    //                 int val = temp[i];
-    //                 temp[i] = temp[j];
-    //                 temp[j] = val;
-    //             }
-    //         }
-    //     }
-    //    return temp;
+    // Approach - 1
+    // Time Complexity - O(n + n^2), Space Complexity - O(n) [Extra space is being used here.]
+        int[] temp = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+           temp[i] = nums[i]*nums[i];
+        }
+        for(int i=0;i<temp.length;i++){
+            for(int j=i+1;j<temp.length;j++){
+                if(temp[i] > temp[j]){
+                    int val = temp[i];
+                    temp[i] = temp[j];
+                    temp[j] = val;
+                }
+            }
+        }
+       return temp;
 
+    // Approach - 2
+     // Time Complexity - O(n), Space Complexity - O(1) [Extra space is not being used here.]
      for(int i=0;i<nums.length;i++){
         nums[i] = nums[i] * nums[i];
      }

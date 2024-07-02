063dde47a24d866e866efe9e093d6c20e44ad83f
Tirtha Sharma <tirthagenze121@gmail.com>
2024-07-02 19:08:55 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (49.25%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        while(n>=2){
+            if(n%2!=0) return false;
+            n=n/2;
+        }
+        return n == 1;
+    }
+}

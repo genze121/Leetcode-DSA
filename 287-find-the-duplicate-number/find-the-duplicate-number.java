a71f828e14a618c4a84357d7f46577c0ab0f554c
Tirtha Sharma <tirthagenze121@gmail.com>
2024-07-04 19:36:29 +0530

    Update find-the-duplicate-number.java
    Updated the approaches.

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,16 +1,18 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        // Set<Integer> set = new HashSet<>();
-        // for(int i=0;i<nums.length;i++){
-        //     if(set.contains(nums[i])){
-        //         return nums[i];
-        //     }
-        //     set.add(nums[i]);
-        // }
+        // Approach - 1
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(set.contains(nums[i])){
+                return nums[i];
+            }
+            set.add(nums[i]);
+        }
 
-        // return -1;
+        return -1;
 
-        int duplicateNumber = 0;
+       // Approach - 2
+       int duplicateNumber = 0;
        Arrays.sort(nums);
         for(int i=0;i<nums.length-1;i++){
             if(nums[i] == nums[i+1]){

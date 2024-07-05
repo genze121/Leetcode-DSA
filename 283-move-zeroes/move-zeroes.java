13d70e5c7111e582afd2d169a8b46af0fdecfe20
Tirtha Sharma <tirthagenze121@gmail.com>
2024-07-05 23:34:38 +0530

    Update move-zeroes.java
    Updated Approaches

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,15 +1,17 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-    //    int counter = 0;
-    //    for(int i=0;i<nums.length;i++){
-    //     if(nums[i]!=0){
-    //         int temp = nums[i];
-    //         nums[i] = nums[counter];
-    //         nums[counter]=temp;
-    //         counter++;
-    //     }
-    //    }
+      // Approach - 1
+       int counter = 0;
+       for(int i=0;i<nums.length;i++){
+        if(nums[i]!=0){
+            int temp = nums[i];
+            nums[i] = nums[counter];
+            nums[counter]=temp;
+            counter++;
+        }
+       }
 
+        // Approach - 2
         for(int i=0;i<nums.length;i++){
             if(nums[i]==0){
                 for(int j=i+1;j<nums.length;j++){

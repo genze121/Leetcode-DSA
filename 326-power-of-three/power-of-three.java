8f5b742779a94093deb0ac46912ae645e22d6c88
Tirtha Sharma <tirthagenze121@gmail.com>
2024-07-12 22:31:59 +0530

    Time: 8 ms (90.32%) | Memory: 44 MB (40.96%) - LeetSync

---
+++ 326-power-of-three/power-of-three.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        // return n > 0 && 1162261467 % n == 0;
+        if(n == 0) return false;
+        while(n!=1){
+            if(n%3!=0) return false;
+            n=n/3;
+        }
+
+        return (n==1);
+    }
+}

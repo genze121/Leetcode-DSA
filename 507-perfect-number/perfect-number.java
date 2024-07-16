59c13ad28e12912a2bfee0ad0fd1e6419513c7e3
Tirtha Sharma <tirthagenze121@gmail.com>
2024-07-16 23:38:38 +0530

    Time: 3 ms (77.19%) | Memory: 40.5 MB (22.50%) - LeetSync

---
+++ 507-perfect-number/perfect-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+     //   int sum = 0;
+    //     for(int i=1;i<num;i++){
+    //         if(num%i == 0){
+    //             sum = sum + i;
+    //         }
+    //     }
+        
+    //     if(sum == num) return true;
+    //     else return false;
+    // }
+    if(num == 0 || num == 1) return false;
+    int sum = 0;
+    for(int i=2;i*i<=num;i++){
+        if(num%i == 0) {
+            sum = sum + i + num/i;
+        }
+    }
+    sum++;
+    return (sum == num);
+}
+}
